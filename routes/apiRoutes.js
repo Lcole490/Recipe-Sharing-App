0fdcc37eff7c9698c75c5769db04a4f0d4ba7054
Aruna Kolli <Kolli_aruna2006@yahoo.co.in>
2019-12-06 20:40:51 -0500

    data tables added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,13 @@
 var db = require("../models");
-
+var dbrecipes = require("./dummy.js").data;
 module.exports = function(app) {
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbrecipes) {
-      res.json(dbrecipes);
-    });
+    // db.Example.findAll({}).then(function(dbrecipes) {
+    //res.json(dbrecipes);
+    //console.log(dbrecipes.length);
+    res.render("recipes", { recipes: dbrecipes });
   });
+  // });
 
   // Get all examples
   app.get("/api/recipes", function(req, res) {
