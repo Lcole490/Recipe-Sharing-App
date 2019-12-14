ac3bcdc8322e94cd48677fec2899c2aaf56860a0
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2019-12-14 09:55:43 -0500

    messing with the authentication

--- config/passport.config.js
+++ config/passport.config.js
@@ -3,33 +3,48 @@ var bcryptjs = require("bcryptjs");
 
 const orm = require("./orm");
 
-function initialize(passport, findUserByEmail, findUserById) {
-  var authenticateUser = async (email, password, done) => {
-    var user = orm.findUserByEmail(email, result => {
-      console.log(result);
-      return result;
-    });
-    console.log(user);
-    if (user == null) {
-      return done(null, false, { message: "No user with that email" });
-    }
+// function initialize(passport, findUserByEmail, findUserById) {
+//   var authenticateUser = async (email, password, done) => {
+  //   var user = findUserByEmail(email, result => {
+  //     console.log(result);
+  //     return result;
+  //   });
+  //   console.log(user);
+  //   if (user == null) {
+  //     return done(null, false, { message: "No user with that email" });
+  //   }
 
-    try {
-      if (await bcryptjs.compare(password, user.password)) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: "Password incorrect" });
-      }
-    } catch (e) {
-      return done(e);
+  //   try {
+  //     if (await bcryptjs.compare(password, user.password)) {
+  //       return done(null, user);
+  //     } else {
+  //       return done(null, false, { message: "Password incorrect" });
+  //     }
+  //   } catch (e) {
+  //     return done(e);
+  //   }
+  // };
+  // authenticateUser
+  
+  passport.use(new LocalStrategy(
+    { usernameField: "email" },
+    function(email, password, done) {
+      orm.users.findUserByEmail(email
+        ).then(function(ormusers){
+          if (!ormusers){
+            return done(null, false, {
+              message: "Incorrect email."
+            });
+          }
+          else if (!ormusers)
+        })
     }
-  };
-  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
+   ));
 
   passport.serializeUser((user, done) => done(null, user.id));
 
   passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id));
+    return done(null, findUserById(id));
   });
 }
 
